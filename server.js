eec0d9ba4f2f7ed75999ad08f6030fea673de041
C. Flores <flores@petalphile.com>
2012-08-10 01:06:53 -0700

    cat gifs!

---
+++ server.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+   http = require('http');
+   util = require('util'),
+   exec = require('child_process').exec;
+
+
+exec('find images').stdout.on('data', function (files) {
+  var cats = files.split("\n");
+  http.createServer(function(req, res){
+    var cat = cats[Math.floor(Math.random()*cats.length)];  
+    fs.readFile(cat, function ( err, img ) {
+      res.writeHead(200, {'Content-Type':'image/gif'});
+      res.end(img, 'binary');
+    });
+  }).listen(process.env.PORT || 8001, '0.0.0.0');
+});
+
+
+
