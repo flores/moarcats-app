18163af2262b365e8ecb2a10b37eaecb3c35fc5a
C. Flores <flores@petalphile.com>
2012-08-13 15:54:01 -0700

    now with accepted CORS

--- README.md
+++ README.md
@@ -4,8 +4,24 @@
 
 cat gifs on demand for any occasion or project. Make any request for any resource for moar cats. No 5xx or 4xx's here: just cat gifs.
 
-Use it anywhere you need a cat gif.  For example
+Use it anywhere you need a cat gif.  For example:
 
 ```<img src='http://lo.no.de' alt='a random cat gif!'/>```
+
+or
+
+```<div id="catcontainer" onclick="moarcats();">
+  <img src="http://lo.no.de" alt="random cat"/>
+</div>
+
+<script type="text/javascript">
+  function moarcats() {
+    var catcontainer = document.getElementById("catcontainer");
+    cat.innerHTML = '<img src="http://lo.no.de" alt="random cat"/>';
+    return false;
+  }
+</script>```
+
+Also allows cross origin requests, nerd.
 
 Inspired by [placekitten](http://placekitten.com).
--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-#!/usr/bin/env node
-
 var fs = require('fs'),
    http = require('http'),
    util = require('util'),
    exec = require('child_process').exec;
 
 
-exec('find images -type f').stdout.on('data', function(files) {
+exec('find images -type f').stdout.on('data', function (files) {
   var cats = files.split("\n");
-  http.createServer( function(req, res) {
-    var cat = cats [Math.floor(Math.random()*cats.length) ];  
-    fs.readFile(cat, function(err, img) {
-      res.writeHead(200, {'Content-Type':'image/gif'});
+  http.createServer(function(req, res){
+    var cat = cats[Math.floor(Math.random()*cats.length)];  
+    fs.readFile(cat, function ( err, img ) {
+      res.writeHead(200, {'Content-Type':'image/gif', 'Access-Control-Allow-Origin':'*'});
       res.end(img, 'binary');
     });
   }).listen(process.env.PORT || 8001, '0.0.0.0');
