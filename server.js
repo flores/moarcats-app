9a6cad50f8e6ea64c35bdefc8d1eac606c51e6bb
C. Flores <flores@petalphile.com>
2012-10-10 22:18:37 -0700

    switch to express to support /random, static cats, and netcat

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 var fs = require('fs'),
-   http = require('http'),
-   util = require('util'),
-   exec = require('child_process').exec;
+  path = require('path'),
+  exec = require('child_process').exec,
+  app = require('express').createServer();
 
-
-exec('find images -type f').stdout.on('data', function (files) {
+exec('find cats -type f').stdout.on('data', function (files) {
   var cats = files.split("\n");
-  http.createServer(function(req, res){
+  
+  app.get('/netcat', function(req, res){
+    res.render('netcat.html');
+  });
+  
+  app.get('/cats/:cat', function(req, res){
+    if (path.existsSync('cats/' + req.params.cat)) {
+      fs.readFile('cats/' + req.params.cat, function ( err, img ) {
+        res.writeHead(200, {'Content-Type':'image/gif', 'Access-Control-Allow-Origin':'*'});
+        res.end(img, 'binary');
+	});
+    }
+    else {
+      res.redirect('/');
+    }
+  });
+
+  app.get('*', function(req, res){
     var cat = cats[Math.floor(Math.random()*cats.length)];  
-    fs.readFile(cat, function ( err, img ) {
-      res.writeHead(200, {'Content-Type':'image/gif', 'Access-Control-Allow-Origin':'*'});
-      res.end(img, 'binary');
-    });
-  }).listen(process.env.PORT || 8001, '0.0.0.0');
+    if (req.url == '/random') {
+      res.send(req.header('host') + '/' + cat);
+    }
+    else {
+      fs.readFile(cat, function ( err, img ) {
+        res.writeHead(200, {'Content-Type':'image/gif', 'Access-Control-Allow-Origin':'*'});
+        res.end(img, 'binary');
+      });
+    }
+  });
+
+  app.listen(8001);
 });
-
-
 
