6ec8be487e33b5bc4388c35c5dba055f99494caf
Ben Lovett <ben@tilderoot.com>
2017-06-02 18:08:10 -0700

    Reorganize helpers.

--- server.rb
+++ server.rb
@@ -3,6 +3,7 @@
 require 'redis'
 require 'short_url'
 require 'sinatra'
+require_relative 'lib/helpers'
 
 set :port, 9894
 set :cat_dir, File.dirname(__FILE__) + '/cats'
@@ -20,44 +21,7 @@ ShortUrl.config do |cfg|
   cfg.type = 'md5'
 end
 
-helpers do
-  def cat_url(cat)
-    "#{settings.cdn_url}/cats/#{cat}"
-  end
-
-  def cat_dir
-    settings.cat_dir
-  end
-
-  def cat_exists?(cat)
-    File.exists?(File.join(cat_dir, cat))
-  end
-
-  def is_short_cat?(cat)
-    ShortUrl.get_url(cat) rescue false
-  end
-
-  def get_all_cats
-    Dir.entries(cat_dir).select {|entry|
-      !File.directory?(File.join(cat_dir, entry))
-    }
-  end
-
-  def get_random_cat
-    all_cats = get_all_cats
-    all_cats[(Random.rand()*all_cats.length).floor]
-  end
-
-  def add_cat_headers
-    headers \
-      "Access-Control-Allow-Origin" => "*"
-  end
-
-  def send_cat(cat)
-    send_file cat
-  end
-
-end
+helpers SinatraHelpers
 
 before do
   headers "X-Moarcats" => "moarcats/#{GIT_REVISION}"
